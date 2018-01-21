cf5778e9958f37af8554af5b298bd4858d0920db
Pavliv <maryan.pavliv@gmail.com>
2018-01-21 16:48:31 +0200

    create landing page

--- script/script.js
+++ script/script.js
@@ -1,7 +1,22 @@
+// Burger menu
 function myFunction(x) {
 	x.classList.toggle("change");
 }
 
-$(document).ready(function(){
-	$('.owl-carousel').owlCarousel();
+
+// Init caroseul
+$(document).ready(function () {
+	var $s = $('.carousel');
+	$s.slick({
+	autoplay: false, 
+	dots: true,
+	arrows: true,
+	pauseOnFocus: false,
+	pauseOnHover: false,
+	});
+});
+// Change symbols for arrows
+$(document).ready(function () {
+	$(".slick-next").text(">");
+	$(".slick-prev").text("<");
 });
